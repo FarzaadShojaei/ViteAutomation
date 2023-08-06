ef20711de8259c791822ef19d4ea412fd1bc039f
f.shojaei <f.shojaei@emofid.com>
2023-08-06 16:22:40 +0330

    KeyboardTyping

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -13,4 +13,17 @@ describe('tasks management', () => {
     cy.get('.backdrop').should('not.exist');
     cy.get('.modal').should('not.exist');
   });
+
+  it('should create a new task', () => {
+    cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click();
+    cy.get('#title').type('New Task');
+    cy.get('#summary').type('Some description');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.backdrop').should('not.exist');
+    cy.get('.modal').should('not.exist');
+    cy.get('.task').should('have.length', 1);
+    cy.get('.task h2').contains('New Task');
+    cy.get('.task p').contains('Some description');
+  });
 });
